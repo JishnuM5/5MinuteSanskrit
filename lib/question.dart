80ab1665aaaa20e8d3bcab78e90e2a104fbb5f72
Jishnu Mehta <jishnumehta@gmail.com>
2024-03-29 16:59:48 -0700

    5 working questions, functionality between pages, and points

---
+++ lib/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  final String question;
+  final List<String> answers;
+  final int correctIndex;
+
+  Question({
+    required this.question,
+    required this.answers,
+    required this.correctIndex,
+  });
+}
