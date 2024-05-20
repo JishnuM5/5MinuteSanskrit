9fba671e591a82c82e94e61c8568d3adc70404b4
Jishnu Mehta <jishnumehta@gmail.com>
2024-05-19 22:43:52 -0700

    Login page, formatting, working on sign up

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -17,14 +17,39 @@ class ProfilePage extends StatelessWidget {
               padding: EdgeInsets.all(10.0),
               child: Divider(),
             ),
-            Text("Signed in as", style: Theme.of(context).textTheme.bodySmall),
-            Text(user!.email!,
-                style: Theme.of(context).textTheme.headlineSmall),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Sign Out'),
-            )
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Text("Signed in as",
+                        style: Theme.of(context).textTheme.bodySmall),
+                    Text(user!.email!,
+                        style: Theme.of(context).textTheme.headlineSmall),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        FirebaseAuth.instance.signOut();
+                      },
+                      child: const Text('Sign out'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
